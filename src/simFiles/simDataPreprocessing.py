ccf6e5fac9bc8d079669a9c03b0cc2d05181b871
JoshuaGensel <joshua.gensel@icloud.com>
2022-11-29 20:39:26 +0100

    testing interaction terms

--- src/simFiles/simDataPreprocessing.py
+++ src/simFiles/simDataPreprocessing.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numba import jit
+from sklearn.preprocessing import PolynomialFeatures
 
 @jit(parallel=True, forceobj=True)
 def convertToMatrix(path : str, maxNumSequences : int, splitProb : float, readSize: int, protConc: int, protConc2 = None, seed = 1) -> np.array:
@@ -99,3 +100,82 @@ def convertToMatrix(path : str, maxNumSequences : int, splitProb : float, readSi
     alignedFragmentsMatrix = np.insert(alignedFragmentsMatrix, 0, protConc, axis = 1).astype(int)
         
     return alignedFragmentsMatrix
+
+
+def convertToInteractionMatrix(path : str, maxNumSequences : int, splitProb : float, readSize: int, protConc: int, protConc2 = None, seed = 1) -> np.array:
+    #set seed
+    np.random.seed(seed)
+     
+    #read in random subset of sequences
+    sequenceArray = np.genfromtxt(path, dtype = str, max_rows = maxNumSequences, delimiter=1)
+            
+    #define sequence length as constant
+    seqLength = len(sequenceArray[0])
+
+    #convert to binary encoding
+    sequenceArray = np.where(sequenceArray == 'C', 1, -1).astype(int)
+    
+    
+    #function that splits a sequence at binomial random points into fragments
+    def splitSequence(seq: str) -> list:
+        #split sequence at random points
+        nSplits = np.random.binomial(seqLength, splitProb)
+        splitPoints = np.random.randint(1, len(seq), nSplits)
+        splitPoints = np.sort(splitPoints)
+        splitPoints = np.insert(splitPoints, 0, 0)
+        splitPoints = np.append(splitPoints, len(seq))
+        fragments = [seq[splitPoints[i] : splitPoints[i+1]] for i in range(len(splitPoints)-1)]
+        
+        return fragments
+
+    #split all sequences into fragments
+    fragmentList = [splitSequence(seq) for seq in sequenceArray]
+    
+    #function that takes fragments and replaces values that are out of read range with 0
+    def replaceOutRange(fragments: list, readRange: int) -> list:
+        readFragments = fragments
+        #if values are more than readRange away from start or end of fragment replace with 0
+        for i in readFragments:
+            if len(i) < readRange*2:
+                pass
+            #replace out of read range values with zeros
+            i[readRange:-readRange] = 0
+        return readFragments
+    
+    #replace all values that are out of read range with 0 in all fragments
+    readFragmentsList = [replaceOutRange(fragments, readSize) for fragments in fragmentList]
+    
+    #function that alignes fragments to sequence
+    def alignFragments(readFragments: list, seqLength: int) -> list:
+    #align sequences and put 0 for missing values
+    #for all fragments add zeros in the beginning for length of all previous fragments
+        alignedFragments = readFragments
+        for i in range(1, len(readFragments)):
+            numZeros = len(readFragments[i-1])
+            alignedFragments[i] = np.insert(readFragments[i], 0, np.zeros(numZeros))
+        # add zeros to the end of all fragments till length of sequence
+        for i in range(len(readFragments)):
+            numZeros = seqLength - len(readFragments[i])
+            alignedFragments[i] = np.append(readFragments[i], np.zeros(numZeros))    
+        
+        return alignedFragments
+    
+    #align all fragments to sequence
+    alignedFragmentsList = [alignFragments(fragments, seqLength) for fragments in readFragmentsList]
+    
+    #create matrix of all aligned fragments
+    alignedFragmentsMatrix = np.stack([item for sublist in alignedFragmentsList for item in sublist], axis = 0)
+    
+    #add interaction terms for every residue to the matrix
+    interaction = PolynomialFeatures(interaction_only=True, include_bias=False)
+    interactionMatrix = interaction.fit_transform(alignedFragmentsMatrix)
+    
+    #if protConc2 is not None add it to the rows
+    if protConc2 != None:
+        interactionMatrix = np.insert(interactionMatrix, 0, protConc2, axis = 1)
+    
+    #add protein concentration to each row
+    interactionMatrix = np.insert(interactionMatrix, 0, protConc, axis = 1).astype(int)
+    
+    return interactionMatrix
+    
