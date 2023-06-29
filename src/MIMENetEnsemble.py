7c7806a9180af75ac7217aeae7d2dc0395802f6e
JoshuaGensel <joshua.gensel@icloud.com>
2023-06-29 04:27:10 +0200

    experimental analysis

--- src/MIMENetEnsemble.py
+++ src/MIMENetEnsemble.py
@@ -146,7 +146,7 @@ def train(training_path, epochs, learning_rate, batch_size, lambda_l2, hidden_si
         #  print("Epoch: " + str(epoch+1) + "/" + str(epochs))
 
         #training loop
-        for x, y in tqdm(train_loader, desc="Epoch: " + str(epo+1) + "/" + str(epochs)):
+        for x, y in tqdm(train_loader, desc="Epoch " + str(epo+1) + "/" + str(epochs), leave=False):
             #cast to device
             x = x.to(device)
             y = y.to(device)
@@ -289,70 +289,131 @@ def inferSingleKds(model, n_protein_concentrations, n_rounds, path_wildtype, n :
     kds_position = []
     prediction_example = np.zeros(n_features)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    for pos, feature in tqdm(enumerate(n_protein_concentrations*n_rounds, n_features, 4)):
-        for mut in range(1,4):
-            wildtype_prediction_example = prediction_example.copy()
-            mutation_prediction_example = prediction_example.copy()
-            wildtype_prediction_example[pos] = 1
-            mutation_prediction_example[pos+mut] = 1
-            wildtype_prediction_example = torch.from_numpy(wildtype_prediction_example).float()
-            mutation_prediction_example = torch.from_numpy(mutation_prediction_example).float()
-            wildtype_prediction_example = wildtype_prediction_example.to(device)
-            mutation_prediction_example = mutation_prediction_example.to(device)
-            with torch.no_grad():
-                wildtype_output = model.predict(wildtype_prediction_example, n)
-                mutation_output = model.predict(mutation_prediction_example, n)
-                #calculate Kds
-                wildtypeKd = 1/np.array(wildtype_output) - 1
-                mutationKd = 1/np.array(mutation_output) - 1
-                correctedKd = mutationKd / wildtypeKd
-                predictedKds.append(correctedKd.tolist())
+    for pos, feature in enumerate(tqdm(range(n_protein_concentrations*n_rounds, n_features, 4))):
+        
+        prediction_example_wildtype = prediction_example.copy()
+        prediction_example_mut1 = prediction_example.copy()
+        prediction_example_mut2 = prediction_example.copy()
+        prediction_example_mut3 = prediction_example.copy()
+
+        # get wildtype at position
+        wildtype_nucleotide = wildtype[pos]
+
+        order_nucleotides = ['A', 'C', 'G', 'T']
+
+        # get index of wildtype nucleotide
+        index_wildtype = order_nucleotides.index(wildtype_nucleotide)
+
+        # set wildtype nucleotide
+        prediction_example_wildtype[feature + index_wildtype] = 1
+
+        # set mutant nucleotides
+        indices_mutants = [i for i in range(4) if i != index_wildtype]
+        prediction_example_mut1[feature + indices_mutants[0]] = 1
+        prediction_example_mut2[feature + indices_mutants[1]] = 1
+        prediction_example_mut3[feature + indices_mutants[2]] = 1
+
+
+        # convert all to tensors
+        prediction_example_wildtype = torch.tensor(prediction_example_wildtype, dtype=torch.float32).to(device)
+        prediction_example_mut1 = torch.tensor(prediction_example_mut1, dtype=torch.float32).to(device)
+        prediction_example_mut2 = torch.tensor(prediction_example_mut2, dtype=torch.float32).to(device)
+        prediction_example_mut3 = torch.tensor(prediction_example_mut3, dtype=torch.float32).to(device)
+
+        # predict
+        with torch.no_grad():
+            wildtype_prediction = model.predict(prediction_example_wildtype, n)
+            mut1_prediction = model.predict(prediction_example_mut1, n)
+            mut2_prediction = model.predict(prediction_example_mut2, n)
+            mut3_prediction = model.predict(prediction_example_mut3, n)
+
+            # calculate kd
+            wildtype_kd = 1 / np.array(wildtype_prediction) - 1
+            mut1_kd = 1 / np.array(mut1_prediction) - 1
+            mut2_kd = 1 / np.array(mut2_prediction) - 1
+            mut3_kd = 1 / np.array(mut3_prediction) - 1
+
+            # correct kds
+            mut1_kd_rel = mut1_kd / wildtype_kd
+            mut2_kd_rel = mut2_kd / wildtype_kd
+            mut3_kd_rel = mut3_kd / wildtype_kd
+
+            # average kds over the 3 mutations
+            pos_kd = np.mean([mut1_kd_rel, mut2_kd_rel, mut3_kd_rel], axis=0)
+            
+            # append to lists
+            kds_nucleotide.append(mut1_kd_rel.tolist())
+            kds_nucleotide.append(mut2_kd_rel.tolist())
+            kds_nucleotide.append(mut3_kd_rel.tolist())
+            kds_position.append(pos_kd.tolist())
 
     return kds_nucleotide, kds_position
 
-def inferPairwiseKds(model, numberFeatures, n : int):
-    """
-    Predicts Kd values for all possible mutations of a pair of positions of the RNA sequence. For this, the model predicts
-    synthetic data where two mutations are present at a time. The order of iterations through the possible mutations is the following:
-    first position, second position, first mutation, second mutation. The predictions are returned as a list of Kd values.
+def inferPairwiseKds(model, n_protein_concentrations, n_rounds, path_wildtype, n : int):
 
-    Args:
-        model (MIMENet.model): Model returned by train function of MIMENet used for prediction
-        numberFeatures (int): Number of features in the dataset. This is 4 times the number of postitions of the RNA (from one 
-        hot encoding) plus the number of protein concentrations times the number of rounds performed (8 for the classical MIME 
-        experiment).
+    # read in wildtype
+    with open(path_wildtype, 'r') as f:
+        wildtype = f.read()
 
-    Returns:
-        list: a list of Kd values for each mutation along the RNA sequence
-    """
-    predictedPairwiseKds = []
-    prediction_example = np.zeros(numberFeatures)
+    n_features = len(wildtype) * 4 + n_protein_concentrations * n_rounds
+    
+    kds_pairwise = []
+    prediction_example = np.zeros(n_features)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    for pos1 in tqdm(range(6, numberFeatures,4)):
-        for pos2 in range(pos1+4, numberFeatures,4):
-            for mut1 in range(1,4):
-                for mut2 in range(1,4):
-                        wildtype_prediction_example = prediction_example.copy()
-                        mutation_prediction_example = prediction_example.copy()
-                        wildtype_prediction_example[pos1] = 1
-                        wildtype_prediction_example[pos2] = 1
-                        mutation_prediction_example[pos1+mut1] = 1
-                        mutation_prediction_example[pos2+mut2] = 1
-                        wildtype_prediction_example = torch.from_numpy(wildtype_prediction_example).float()
-                        mutation_prediction_example = torch.from_numpy(mutation_prediction_example).float()
-                        wildtype_prediction_example = wildtype_prediction_example.to(device)
-                        mutation_prediction_example = mutation_prediction_example.to(device)
-                        #get output binding probabilities
-                        with torch.no_grad():
-                            wildtype_prediciton = model.predict(wildtype_prediction_example, n)
-                            mutation_prediciton = model.predict(mutation_prediction_example, n)
-                            #calculate Kd and append list
-                            wildtypeKd = 1/np.array(wildtype_prediciton) - 1
-                            mutationKd = 1/np.array(mutation_prediciton) - 1
-                            correctedKd = mutationKd / wildtypeKd
-                            predictedPairwiseKds.append(correctedKd.tolist())
+    for pos1, feature1 in enumerate(range(n_protein_concentrations*n_rounds, n_features, 4)):
+        for pos2, feature2 in enumerate(tqdm(range(feature1+4, n_features, 4), desc='Position ' + str(pos1+1) + '/' + str(len(wildtype)) + ' ', leave=False)):
+            
+            # get wildtype at both positions
+            wildtype1 = wildtype[pos1]
+            wildtype2 = wildtype[pos2]
 
-    return predictedPairwiseKds
+            order_nucleotides = ['A', 'C', 'G', 'T']
+
+            index_wildtype1 = order_nucleotides.index(wildtype1)
+            index_wildtype2 = order_nucleotides.index(wildtype2)
+
+            # iterate over all possible mutations
+            for mut1 in range(4):
+                for mut2 in range(4):
+
+                    # skip wildtype
+                    if mut1 == index_wildtype1 or mut2 == index_wildtype2:
+                        continue
+
+                    prediction_example_wildtype = prediction_example.copy()
+                    prediction_example_mutant = prediction_example.copy()
+
+                    # set wildtype
+                    prediction_example_wildtype[feature1+index_wildtype1] = 1
+                    prediction_example_wildtype[feature2+index_wildtype2] = 1
+
+                    # set mutant
+                    prediction_example_mutant[feature1+mut1] = 1
+                    prediction_example_mutant[feature2+mut2] = 1
+
+                    # print('wildtypes: ', feature1+index_wildtype1, feature2+index_wildtype2)
+                    # print('mutants: ', feature1+mut1, feature2+mut2)
+
+                    # convert to tensor
+                    prediction_example_wildtype = torch.tensor(prediction_example_wildtype, dtype=torch.float32).to(device)
+                    prediction_example_mutant = torch.tensor(prediction_example_mutant, dtype=torch.float32).to(device)
+
+                    # predict
+                    with torch.no_grad():
+                        prediction_wildtype = model.predict(prediction_example_wildtype, n)
+                        prediction_mutant = model.predict(prediction_example_mutant, n)
+
+                    # calculate kd
+                    wildtype_kd = 1 / np.array(prediction_wildtype) - 1
+                    mutant_kd = 1 / np.array(prediction_mutant) - 1
+
+                    # calculate relative kd
+                    relative_kd = mutant_kd / wildtype_kd
+
+                    # append to list
+                    kds_pairwise.append(relative_kd.tolist())
+
+    return kds_pairwise
 
 def inferSingleKdsProtein(model, numberFeatures, n : int, number_protein_concentrations : int):
     
@@ -405,6 +466,7 @@ def inferEpistasis(singleKds : list, pairwiseKds : list):
 
     #iterate through all possible pairs
     epistasis = []
+    pairs = []
     i = 0
     for pos1 in range(single_kd_pred.shape[0], 3):
         for pos2 in range(pos1+1, single_kd_pred.shape[0], 3):
@@ -431,6 +493,41 @@ def inferEpistasis(singleKds : list, pairwiseKds : list):
                     else:
                         epistasis.append(0)
                     i += 1
+                    pairs.append((pos1+mut1, pos2+mut2))
 
-    return epistasis
+    return epistasis, pairs
 
+def computeEpistasis(singleKds : list, pairwiseKds : list):
+    
+    single_kd_pred = np.array(singleKds)
+    single_kd_pred_means = np.mean(single_kd_pred, axis=1)
+    single_kd_pred_conf = np.zeros((single_kd_pred_means.shape[0], 2))
+    for i in range(single_kd_pred_means.shape[0]):
+        single_kd_pred_conf[i] = np.quantile(single_kd_pred[i], [0.025, 0.975])
+
+    pairwise_kd_pred = np.array(pairwiseKds)
+    pairwise_kd_pred_means = np.mean(pairwise_kd_pred, axis=1)
+    pairwise_kd_pred_conf = np.zeros((pairwise_kd_pred_means.shape[0], 2))
+    for i in range(pairwise_kd_pred_means.shape[0]):
+        pairwise_kd_pred_conf[i] = np.quantile(pairwise_kd_pred[i], [0.025, 0.975])
+
+    #iterate through all possible pairs
+    epistasis_nucleotides = []
+    epistasis_positions = []
+    nucleotide_pairs = []
+    position_pairs = []
+    j = 0
+    for pos1 in range(0,single_kd_pred.shape[0], 3):
+        for pos2 in range(pos1+3, single_kd_pred.shape[0], 3):
+            for mut1 in range(3):
+                for mut2 in range(3):
+                    epistasis_nucleotides.append((single_kd_pred_means[pos1+mut1]*single_kd_pred_means[pos2+mut2])/pairwise_kd_pred_means[j])
+                    j += 1
+                    nucleotide_pairs.append((pos1+mut1, pos2+mut2))
+            
+            # compute average epistasis for each mutation per pos 1 and pos 2
+            epistasis_positions.append(np.mean(epistasis_nucleotides[-9:]))
+            position_pairs.append((pos1//3, pos2//3))
+            
+
+    return epistasis_nucleotides, nucleotide_pairs, epistasis_positions, position_pairs
